c85b6ae31ddd2a9cee64ffc55fa4f2c60888d4f6
Jakub Chodorowicz <jakub.chodorowicz@gmail.com>
2019-04-10 00:32:54 +0200

    initial config of gatsby - typescript, emotion

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import styled from "@emotion/styled"
+
+const Container = styled.div`
+  color: white;
+  background-color: rebeccapurple;
+`
+
+export default () => <Container>Hello Jan!</Container>
