87293bb0e1aa322e9555332e990b8ceb7524e678
Jakub Chodorowicz <jakub.chodorowicz@gmail.com>
2019-05-06 00:35:01 +0200

    use generated type

--- src/templates/home-page.tsx
+++ src/templates/home-page.tsx
@@ -5,15 +5,10 @@ import { Layout } from "../layout"
 import { SectonPhotoText, SectionBooks, SectionPapers, SectionContact } from "./home";
 import { SectionTop } from "../components/section-top";
 import HomePageImage from "../images/home-page-bg@2x.jpg"
+import { HomePageTemplateQuery } from "../generated/graphql"
 
 interface IProps {
-  data: {
-    markdownRemark: {
-      frontmatter: {
-        claim: string;
-      }
-    }
-  }
+  data: HomePageTemplateQuery
 }
 
 const HomePage: React.FC<IProps> = (props) => {
