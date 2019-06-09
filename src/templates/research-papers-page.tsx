eb274bbb945012d43d1502732a95c5ad17920ded
Jakub Chodorowicz <jakub.chodorowicz@gmail.com>
2019-06-09 21:27:24 +0200

    Add categories to research papers

--- src/templates/research-papers-page.tsx
+++ src/templates/research-papers-page.tsx
@@ -1,28 +1,13 @@
 import React from "react"
 import { graphql } from "gatsby"
-import { uniq } from "lodash"
 
 import { Layout } from "../layout"
 import { SectionTop } from "../components"
 import ResearchPageBackground from "../images/research/research-bg@2x.jpg"
 import { SectionResearchPapers } from "./research-papers/section-reasearch-papers"
 
-interface IPaper {
-  node: {
-    frontmatter: {
-      category: string
-    }
-  }
-}
-
-function getCategories(papers: IPaper[]): string[] {
-  const categories = papers.map(paper => paper.node.frontmatter.category)
-
-  return uniq(categories)
-}
-
 const ResearchPapersPage: React.FC = ({ data, pageContext }) => {
-  const categories = getCategories(pageContext.researchPapers);
+  const { categories } = data.markdownRemark.frontmatter;
   return (
     <Layout>
       <SectionTop background={ResearchPageBackground} isHome={false} />
@@ -48,6 +33,7 @@ export const pageQuery = graphql`
         title
         category
         where
+        categories
       }
     }
   }
