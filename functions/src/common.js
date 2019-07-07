5831322c6b5ecae86175d28ae347abbb2901f3b7
Jakub Chodorowicz <jakub.chodorowicz@gmail.com>
2019-07-07 15:05:50 +0200

    Remove obsolete functions

--- functions/src/common.js
+++ functions/src/common.js
@@ -1,35 +1,10 @@
-const domain = "https://walulik.aero"
-
-function getAutoRespondMessageContent() {
-  return `
-  <p>Thank you for taking the time to contact me. Your message has been successfully sent. I will get back to you soon.</p>
-  <p>Kind regards,</p>
-
-  <p>
-  Jan Walulik<br/>
-  Aviation Research & Consulting<br/>
-  50/515 Nowogrodzka St.<br/>
-  Warsaw, PL00-695<br/>
-  Poland<br/>
-  jan@walulik.aero<br/>
-  <a href="${domain}">walulik.aero</a><br/>
-  </p>
-
-  <p>
-    Please be informed that any personal data supplied may be processed according to our privacy policy rules.</a>. 
-  </p>
-  `
-}
-
 const statusCode = 200
-
 const headers = {
   "Access-Control-Allow-Origin": "*",
   "Access-Control-Allow-Headers": "Content-Type",
 }
 
 module.exports = {
-  getAutoRespondMessageContent,
   statusCode,
   headers
 }
